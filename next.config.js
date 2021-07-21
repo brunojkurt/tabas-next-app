317d1a767b08faf4f5c98dde29b60b8c362e0903
Bruno Kurt <brunojkurt@gmail.com>
2021-07-21 01:29:45 -0300

    update config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
-import {
+const {
   PHASE_DEVELOPMENT_SERVER,
   PHASE_PRODUCTION_BUILD
-} from 'next/constants'
+} = require('next/constants')
 
 module.exports = (phase) => {
   // when started in development mode `next dev` or `npm run dev` regardless of the value of STAGING environmental variable
@@ -13,7 +13,8 @@ module.exports = (phase) => {
   const isStaging = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === '1'
 
   const env = {
-    API_URL: 'http://localhost'
+    API_URL_EN: 'https://b-dev.tabas.com/api/v1',
+    API_URL_PT: 'https://b-dev.tabas.com.br/api/v1'
   }
 
   return {
